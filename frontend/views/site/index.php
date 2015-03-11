45596c64d2d50f8af2e226612d4ac20002740fe8
Student <Student@10.8.102.152>
2015-03-11 15:29:17 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,5 +1,19 @@
 <?php
+use yii\helpers\Html;
 /* @var $this yii\web\View */
 $this->title = 'กรมอนามัย';
 ?>
-ๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆๆ
+<?php
+$route1=Yii::$app->urlManager->createUrl('test/test1');
+?>
+<a href="<?=$route1?>">ไปที่ test 1</a><br>
+
+<?php
+$route2=Yii::$app->urlManager->createUrl(['test/test2','name'=>'HPC1','lname'=>'Anamai']);
+?>
+<a href="<?=$route2?>">ไปที่ test 2</a><br>
+
+<?=  
+Html::a ('ลิงค์แบบที่ 3',['test/test1','a'=>'1'])
+
+?>
