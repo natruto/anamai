d80f150c06744b5c653c05d3dfd41eb035099f8c
Student <Student@10.8.102.152>
2015-03-11 15:55:54 +0700

    Merge origin/master
    Conflicts:
    	README.md

---
+++ frontend/assets/MaterialAsset.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class MaterialAsset extends AssetBundle
+{
+    public $basePath = '@themes/material';
+    public $baseUrl = '@themes';
+    public $css = [
+        'css/material-wfont.min.css',
+        'css/material.min.css',
+        'css/ripples.min.css',
+        'css/style.css',
+    ];
+    public $js = [
+        'js/material.min.js',
+        'js/ripples.min.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
